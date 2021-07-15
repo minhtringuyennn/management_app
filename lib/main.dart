0751860a60f7c0a983ab1c41c2d568d5ce5ebcd6
MinhTriNguyen <minhtri.minhzzxzx@gmail.com>
2021-07-15 16:07:26 +0700

    App template

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:management_app/screens/base_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Management App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Poppins",
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  const BaseScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
